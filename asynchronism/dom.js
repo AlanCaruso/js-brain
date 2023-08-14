de65cead193e1744a66193bb62834bd8d9defc47
Alan Caruso <alan.carusoo@gmail.com>
2023-08-14 12:12:45 -0300

    complete loading dom

--- asynchronism/dom.js
+++ asynchronism/dom.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOM fully loaded and parsed");
 });
+
+function doSomething() {
+  console.info("DOM loaded");
+}
+
+if (document.readyState === "loading") {
+  // Loading hasn't finished yet
+  document.addEventListener("DOMContentLoaded", doSomething);
+} else {
+  // `DOMContentLoaded` has already fired
+  doSomething();
+}
